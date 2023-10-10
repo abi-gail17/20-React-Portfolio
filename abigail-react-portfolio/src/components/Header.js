fce17b53f15fe323b425af0d541c142b2ee5c916
Abigail <cinco.de.abby@gmail.com>
2023-10-10 18:30:42 -0400

    made components for all the pages and some styling for header and footer

---
+++ abigail-react-portfolio/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MyLogo from "../assets/mylogo.png";
+import "../assets/style/style.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <Link to="/" className="header-home">
+              <img
+                src={MyLogo}
+                alt="Abigail's Logo"
+                className="header-logo"
+                />
+                <h1>Abigail Cinco</h1>
+            </Link>
+            <nav>
+                <ul className="nav-bar">
+                    <li className="nav-links">
+                        <Link to="/">About Me</Link>
+                    </li>
+                    <li className="nav-links">
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li className="nav-links">
+                        <Link to="/resume">Resume</Link>
+                    </li>
+                    <li className="nav-links">
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
